d662fbd62ec05f63fcd1fcdefe9a70bfa264f1fc
sadko4u <sadko4u@gmail.com>
2020-09-18 23:59:28 +0300

    Updated build files

--- dependencies.mk
+++ dependencies.mk
@@ -1,25 +1,45 @@
+#
+# Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
+#           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+#
+# This file is part of lsp-ws-lib
+#
+# lsp-ws-lib is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# lsp-ws-lib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lsp-ws-lib.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 # Variables that describe dependencies
-LSP_COMMON_LIB_VERSION     := 1.0.7
+LSP_COMMON_LIB_VERSION     := 1.0.8
 LSP_COMMON_LIB_NAME        := lsp-common-lib
 LSP_COMMON_LIB_TYPE        := src
 LSP_COMMON_LIB_URL         := https://github.com/sadko4u/$(LSP_COMMON_LIB_NAME).git
 
-LSP_LLTL_LIB_VERSION       := 0.5.2
+LSP_LLTL_LIB_VERSION       := 0.5.3
 LSP_LLTL_LIB_NAME          := lsp-lltl-lib
 LSP_LLTL_LIB_TYPE          := src
 LSP_LLTL_LIB_URL           := https://github.com/sadko4u/$(LSP_LLTL_LIB_NAME).git
 
-LSP_RUNTIME_LIB_VERSION    := 0.5.3
+LSP_RUNTIME_LIB_VERSION    := 0.5.4
 LSP_RUNTIME_LIB_NAME       := lsp-runtime-lib
 LSP_RUNTIME_LIB_TYPE       := src
 LSP_RUNTIME_LIB_URL        := https://github.com/sadko4u/$(LSP_RUNTIME_LIB_NAME).git
 
-LSP_R3D_BASE_LIB_VERSION   := 0.5.2
+LSP_R3D_BASE_LIB_VERSION   := 0.5.3
 LSP_R3D_BASE_LIB_NAME      := lsp-r3d-base-lib
 LSP_R3D_BASE_LIB_URL       := https://github.com/sadko4u/$(LSP_R3D_BASE_LIB_NAME).git
 LSP_R3D_BASE_LIB_TYPE      := hdr
 
-LSP_TEST_FW_VERSION        := 1.0.5
+LSP_TEST_FW_VERSION        := 1.0.6
 LSP_TEST_FW_NAME           := lsp-test-fw
 LSP_TEST_FW_TYPE           := src
 LSP_TEST_FW_URL            := https://github.com/sadko4u/$(LSP_TEST_FW_NAME).git
--- project.mk
+++ project.mk
@@ -1,9 +1,30 @@
+#
+# Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
+#           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+#
+# This file is part of lsp-ws-lib
+#
+# lsp-ws-lib is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# lsp-ws-lib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lsp-ws-lib.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 # Package version
 ARTIFACT_NAME               = lsp-ws-lib
+ARTIFACT_DESC               = LSP window subsystem core library
 ARTIFACT_VARS               = LSP_WS_LIB
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_EXPORT_ALL         = 1
-ARTIFACT_VERSION            = 0.5.2-devel
+ARTIFACT_VERSION            = 0.5.2
 
 # List of dependencies
 TEST_DEPENDENCIES = \
@@ -16,6 +37,16 @@ DEPENDENCIES = \
   LSP_R3D_BASE_LIB \
   LSP_RUNTIME_LIB
 
+LINUX_DEPENDENCIES = \
+  LIBSNDFILE \
+  XLIB \
+  CAIRO
+  
+BSD_DEPENDENCIES = \
+  LIBSNDFILE \
+  XLIB \
+  CAIRO
+
 # For Linux-based systems, use libsndfile and xlib
 ifeq ($(PLATFORM),Linux)
   DEPENDENCIES             += LIBSNDFILE XLIB CAIRO
@@ -25,3 +56,10 @@ endif
 ifeq ($(PLATFORM),BSD)
   DEPENDENCIES             += LIBSNDFILE XLIB CAIRO
 endif
+
+ALL_DEPENDENCIES = \
+  $(DEPENDENCIES) \
+  $(TEST_DEPENDENCIES) \
+  $(LINUX_DEPENDENCIES) \
+  $(BSD_DEPENDENCIES)
+  
