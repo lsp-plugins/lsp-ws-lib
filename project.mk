e5f9dc9bd3bd8730bf62cc480e774b4f5a148f92
sadko4u <sadko4u@gmail.com>
2020-11-13 23:52:23 +0300

    Version up

--- project.mk
+++ project.mk
@@ -24,7 +24,7 @@ ARTIFACT_DESC               = LSP window subsystem core library
 ARTIFACT_VARS               = LSP_WS_LIB
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_EXPORT_ALL         = 1
-ARTIFACT_VERSION            = 0.5.3
+ARTIFACT_VERSION            = 0.5.4-devel
 
 # List of dependencies
 TEST_DEPENDENCIES = \
