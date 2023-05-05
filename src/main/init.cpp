835b8955ec528413cfa7019006c19169bbb9e699
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-05-05 20:29:52 +0300

    The library now requires to call ws::init() before proceeding with other operations

---
+++ src/main/init.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2023 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2023 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-ws-lib
+ * Created on: 5 мая 2023 г.
+ *
+ * lsp-ws-lib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-ws-lib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-ws-lib. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <lsp-plug.in/dsp/dsp.h>
+#include <lsp-plug.in/ws/ws.h>
+
+namespace lsp
+{
+    namespace ws
+    {
+        LSP_WS_LIB_PUBLIC
+        void init()
+        {
+            dsp::init();
+        }
+    } /* namespace ws */
+} /* namespace lsp */
+
+
