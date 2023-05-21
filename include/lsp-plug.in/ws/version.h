79c0e371aabe2f096f20d36287a41c241be866d6
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-05-21 15:43:41 +0300

    Version up

--- include/lsp-plug.in/ws/version.h
+++ include/lsp-plug.in/ws/version.h
@@ -24,7 +24,7 @@
 
 #define LSP_WS_LIB_MAJOR            1
 #define LSP_WS_LIB_MINOR            0
-#define LSP_WS_LIB_MICRO            11
+#define LSP_WS_LIB_MICRO            12
 
 #if defined(LSP_WS_LIB_PUBLISHER)
     #define LSP_WS_LIB_PUBLIC               LSP_EXPORT_MODIFIER
