196ed3468ae6cf1a4bd0fd75f6accacf33a567a0
Vladimir Sadovnikov <sadko4u@gmail.com>
2025-06-27 01:22:31 +0300

    Reducing size of code

--- include/private/gl/Batch.h
+++ include/private/gl/Batch.h
@@ -188,6 +188,14 @@ namespace lsp
                     ssize_t htriangle(uint32_t a, uint32_t b, uint32_t c);
 
                     /**
+                     * Add indices that generate triangle fan chain
+                     * @param v0i index of the first vertex
+                     * @param count number of triangles
+                     * @return absolute index of record in index buffer or negative error code
+                     */
+                    ssize_t htriangle_fan(uint32_t v0i, uint32_t count);
+
+                    /**
                      * Add rectangle with hint
                      * @param a index of the first vertex
                      * @param b index of the second vertex
